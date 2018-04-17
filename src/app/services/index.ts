8e89d6fabd2eff20ddb251507a8e679031540103
topdevman <softdevman75@gmail.com>
2018-04-17 18:18:51 +0200

    first commit

---
+++ src/app/services/index.ts
@@ -0,0 +1,7 @@
+export * from './authentication.service';
+export * from './search.service';
+export * from './window.service';
+export * from './targets.service';
+export * from './cases.service';
+export * from './audit-trail.service';
+export * from './users.service';
